5d862ddc22ea4d81f59914f4b86f6da00554f672
Imogen904 <imogen.denis1@students.mq.edu.au>
2025-08-27 16:57:27 +1000

    Week5 Tasks

--- src/Actor.java
+++ src/Actor.java
@@ -1,14 +1,30 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.Polygon;
 
 public abstract class Actor {
   Color color;
+  List<Polygon> polygons = new ArrayList<>();
   Cell loc;
 
   public void paint(Graphics g) {
-    g.setColor(color);
+    g.setColor(Color.BLACK);
+    /*
     g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
     g.setColor(Color.GRAY);
     g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+     */
+    for(int i = 0; i < polygons.size(); i++) {
+      g.drawPolygon(polygons.get(i));
+    }
   }
 }
+
+
+/*We will use `ArrayList` again!  Have the `Color` field of `Actor` changed to 
+a list of polygons and initialise each subclass to an appropriate set of polygons.  
+You might find the following polygons a useful starting point where `location` 
+is the top-left point of the actor (but I am sure you can do better as well - 
+share your designs on the forums!): */
--- src/Stage.java
+++ src/Stage.java
@@ -1,23 +1,41 @@
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Stage {
   Grid grid;
   Actor cat;
   Actor dog;
   Actor bird;
+  List<Actor> listOfActors;
 
   public Stage() {
     grid = new Grid();
     cat = new Cat(grid.cellAtColRow(0, 0));
     dog = new Dog(grid.cellAtColRow(0, 15));
     bird = new Bird(grid.cellAtColRow(12, 9));
+    listOfActors = new ArrayList<>();
+    listOfActors.add(cat);
+    listOfActors.add(dog);
+    listOfActors.add(bird);
+
   }
 
   public void paint(Graphics g, Point mouseLoc) {
     grid.paint(g, mouseLoc);
-    cat.paint(g);
+    /*cat.paint(g);
     dog.paint(g);
-    bird.paint(g);
+    bird.paint(g);*/
+    
+    for(int i = 0; i < listOfActors.size(); i++) {
+      listOfActors.get(i).paint(g);
+    }
+    Integer xpos = grid.cellAtPoint(mouseLoc).get().x;
+    Integer ypos = grid.cellAtPoint(mouseLoc).get().y;
+
+    g.drawString("X Position: " + xpos.toString(), 750, 200);
+    g.drawString("Y Position: " + ypos.toString(), 750, 250);
+
   }
 }
